0e7533a70220acd1e76c118d36ea323ab1335fc7
oktay <oktay@OKTAY>
2022-08-15 16:03:03 +0200

    Add project files.

---
+++ OMA_ProjectManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OMA_ProjectManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
